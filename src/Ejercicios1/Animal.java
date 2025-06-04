f2d72ce97655462fa7c05fd3606148e6c75e829e
Branko <villaverdezaratebranko@gmail.com>
2025-06-04 20:58:46 -0300

    Primer Ejercicio agregado

---
+++ src/Ejercicios1/Animal.java
@@ -0,0 +1,11 @@
+package Ejercicios1;
+
+public class Animal {
+    public
+    String especie;
+    int edad;
+
+    public void hacerSonido(){
+        System.out.println("el " + especie + " hacer guau");
+    }
+}
--- src/Ejercicios1/Main.java
+++ src/Ejercicios1/Main.java
@@ -3,8 +3,20 @@ package Ejercicios1;
 public class Main{
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Persona a = new Persona("eee",1);
-        a.setNombre("culo");
-        System.out.println(a.getNombre());
+        //Persona a = new Persona("Juan",1);
+        //a.setNombre("NJorge");
+        //System.out.println(a.getNombre());
+        //Sin uso de constructor
+        Animal a1 = new Animal();
+        Animal a2 = new Animal();
+
+        a1.edad = 5;
+        a1.especie = "leon";
+        a2.edad = 10;
+        a2.especie = "tigre";
+
+        a1.hacerSonido();
+        a2.hacerSonido();
+
     }
 }
